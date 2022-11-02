ce54362d291a5d98f0928ebcdd00e9136cd9537a
dywjd12 <dywjd3045@gmail.com>
2022-11-02 21:04:39 +0900

    별 찍는 중

---
+++ ZeroCho/let's-get-it-javascript-Renewal/code/4.star.js
@@ -0,0 +1,28 @@
+// 별찍기
+
+// 1번
+// *
+// **
+// ***
+// ****
+// *****
+let star1 = "";
+for (let i = 0; i < 5; i++) {
+  // star = "*" + star;
+  star1 += "*";
+  console.log(star1);
+}
+
+// 2번
+// *****
+// ****
+// ***
+// **
+// *
+// let star2 = "";
+// for (let j = 4; j > 0; j - 1) {
+//   star2 += "*";
+//   console.log(j);
+//   console.log(star2);
+// }
+console.log();
